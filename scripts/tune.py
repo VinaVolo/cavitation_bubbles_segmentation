08304f9735c95e24b1dae728ebd851d9dae2f718
VinaVolo <krotkov.na@gmail.com>
2026-04-09 14:03:14 +0300

    fix: add line breaks to best_hp and best_metric names in ClearML summary

--- scripts/tune.py
+++ scripts/tune.py
@@ -291,10 +291,11 @@ def main() -> None:
     clearml_logger = task.get_logger()
     for k, v in best_config.items():
         if isinstance(v, (int, float)):
-            clearml_logger.report_single_value(f"best_hp/{k}", round(v, 3))
+            clearml_logger.report_single_value(f"best_hp/\n{k}", round(v, 3))
     for k, v in best_metrics.items():
         if isinstance(v, (int, float)):
-            clearml_logger.report_single_value(f"best_metric/{k}", round(v, 3))
+            title = f"best_metric/\n{k.replace('/', '/\n')}"
+            clearml_logger.report_single_value(title, round(v, 3))
 
     best_hp_path = Path("models", "tuned") / "best_hyperparams.yaml"
     best_hp_path.parent.mkdir(parents=True, exist_ok=True)
